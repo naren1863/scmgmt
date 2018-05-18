2e8878feac95c4f597b9f17bc76e01b35afe5920
Piyush Kumar Agrawal <PiAgrawal@manh.com>
2018-05-18 16:58:20 +0530

    New Login Page

--- web/src/providers/login-service.ts
+++ web/src/providers/login-service.ts
@@ -4,11 +4,16 @@ import { Injectable } from '@angular/core';
 export class LoginService {
 
     logged: boolean = false;
+    username: string ="";
     isLoggedIn() {
         return this.logged;
     }
 
     setLogin(loggedIn){
         this.logged = loggedIn;
+    }
+
+    getUserName(){
+
     }
 }
